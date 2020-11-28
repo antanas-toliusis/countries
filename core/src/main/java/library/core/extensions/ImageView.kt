30fde986974318fe558363ef8b88645b7c9e9399
Antanas Toliusis <antanas.toliusis1@gmail.com>
2020-11-28 08:54:33 +0000

    Placeholder set up for flag image

--- core/src/main/java/library/core/extensions/ImageView.kt
+++ core/src/main/java/library/core/extensions/ImageView.kt
@@ -3,10 +3,12 @@ package library.core.extensions
 import android.net.Uri
 import android.widget.ImageView
 import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
+import library.core.R
 
 fun ImageView.loadSwg(url: String) {
     GlideToVectorYou
         .init()
         .with(context)
+        .setPlaceHolder(R.drawable.ic_baseline_flag_24, R.drawable.ic_baseline_flag_24)
         .load(Uri.parse(url), this)
 }
